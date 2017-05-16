5680422a7919e212045ac89d74a71eb4f36038db
Jim Pepe <jim.pepe@verizon.net>
2017-05-16 15:01:01 -0400

    Initial commit

---
+++ app/src/main/java/com/course/example/xmldraw/XMLDraw.java
@@ -0,0 +1,13 @@
+package com.course.example.xmldraw;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class XMLDraw extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.xmldrawable);
+    }
+}
